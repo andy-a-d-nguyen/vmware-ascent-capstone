76a151a938083e0f3c675f455bc60672f42bdbb2
Andy Nguyen <a.d.nguyen.andy@gmail.com>
2021-06-07 13:50:39 -0700

    Created pipeline file, Dockerfile and empty UsersController.

---
+++ src/main/java/com/galvanize/useraccounts/UsersController.java
@@ -0,0 +1,7 @@
+package com.galvanize.useraccounts;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UsersController {
+}
---
+++ src/test/java/com/galvanize/useraccounts/UsersControllerTests.java
@@ -0,0 +1,4 @@
+package com.galvanize.useraccounts;
+
+public class UsersControllerTests {
+}
