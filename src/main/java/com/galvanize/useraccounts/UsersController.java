e589662ba9dfad3d05a889087e637d1df15f2fe5
Raf-Batista <RafaelB2424@gmail.com>
2021-06-08 13:27:40 -0400

    Add endpoint to create a new user

--- src/main/java/com/galvanize/useraccounts/UsersController.java
+++ src/main/java/com/galvanize/useraccounts/UsersController.java
@@ -1,7 +1,24 @@
 package com.galvanize.useraccounts;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class UsersController {
+    /*
+    update, create, show (returns a user), index (return all users)
+    */
+    UsersService usersService;
+
+    public UsersController(UsersService usersService) {
+        this.usersService = usersService;
+    }
+
+    @PostMapping("/users")
+    public User createUser(@RequestBody User user){
+        return usersService.createUser(user);
+    }
 }
