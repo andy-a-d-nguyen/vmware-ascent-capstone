4df9774452ab13afe6274d90155ece8d7765f957
chaomonica <monica.y.chao@gmail.com>
2021-06-08 11:11:38 -0500

    added fields for Users class

---
+++ src/main/java/com/galvanize/useraccounts/Users.java
@@ -0,0 +1,11 @@
+package com.galvanize.useraccounts;
+
+public class Users {
+    private Long id;
+    private String username;
+    private String password;
+    private String email;
+    private String address;
+    private String creditCard;
+    private boolean verified;
+}
