00b4d35fdf8c01233dd3a8982c25115817a914de
Yvonne <yvonnersz@gmail.com>
2021-06-09 10:08:40 -0700

    Create getters and setters for firstName and lastName

--- src/main/java/com/galvanize/useraccounts/User.java
+++ src/main/java/com/galvanize/useraccounts/User.java
@@ -45,6 +45,30 @@ public class User {
         this.username = username;
     }
 
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
     public String getPassword() {
         return password;
     }
