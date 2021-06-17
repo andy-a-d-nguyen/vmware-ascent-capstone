96e40d1b9f8cb31f3e698a836879c8bdd041d0e5
Yvonne <yvonnersz@gmail.com>
2021-06-17 08:33:35 -0700

    Create deploy, secret, and service yaml files

---
+++ secret.sh
@@ -0,0 +1,5 @@
+kubectl create secret docker-registry user-accounts-feature-deploy-token \
+--docker-username=gitlab+deploy-token-488296 \
+--docker-password=h9RqXpxoDM8VbPP2gEdF \
+--docker-email=phamyvonne92@gmail.com \
+--docker-server=registry.gitlab.com
