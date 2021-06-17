6a7b9945df5e0847947bd4640c02912e710870ed
Yvonne <yvonnersz@gmail.com>
2021-06-17 12:13:53 -0700

    Remove sensitive information

--- secret.sh
+++
@@ -1,5 +0,0 @@
-kubectl create secret docker-registry user-accounts-feature-deploy-token \
---docker-username=gitlab+deploy-token-488296 \
---docker-password=h9RqXpxoDM8VbPP2gEdF \
---docker-email=phamyvonne92@gmail.com \
---docker-server=registry.gitlab.com
